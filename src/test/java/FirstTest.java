ffe1eb433dc60c52558e0a50a18171ac60611413
stepk <stepan.zagurenko@gmail.com>
2025-08-03 18:39:53 +0300

    added build.gradle

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,2 +1,36 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
 public class FirstTest {
-}
+    @BeforeAll
+    static void setup() {
+        WebDriverManager.chromedriver().setup();
+        Configuration.browserSize = "1920x1080";
+        Configuration.timeout = 15000;
+        Configuration.pageLoadTimeout = 60000;
+    }
+
+        @Test
+        void FirstTest1() {
+
+            open("https://demoqa.com/text-box");
+            $("#userName").shouldBe(visible);
+
+            $("[#userName]").setValue("Alesha");
+            $("[#userEmail]").setValue("Popov");
+            $("[#currentAddress]").setValue("Yaroslavskiy 95");
+            $("[#permanentAddress]").setValue("Yaroslavskiy 9000");
+            $("submit").click();
+
+        }
+    }
+
+
+
+
+
